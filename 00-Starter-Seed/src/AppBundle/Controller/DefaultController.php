9a18bf4f108f76cc533e1244c84d2362d63566e6
Alexis Luque <aluque@indexlabs.com.uy>
2017-10-24 12:26:18 -0300

    Remove auth0.js

--- 00-Starter-Seed/src/AppBundle/Controller/DefaultController.php
+++ 00-Starter-Seed/src/AppBundle/Controller/DefaultController.php
@@ -2,7 +2,6 @@
 
 namespace AppBundle\Controller;
 
-use Dotenv\Dotenv;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -14,13 +13,7 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        $dotenv = new Dotenv(__DIR__ . '/../../..');
-        $dotenv->load();
-        return $this->render('index.html.twig', array(
-            'AUTH0_DOMAIN' => getenv('AUTH0_DOMAIN'),
-            'AUTH0_CLIENT_ID' => getenv('AUTH0_CLIENT_ID'),
-            'AUTH0_CLIENT_SECRET' => getenv('AUTH0_CLIENT_SECRET')
-        ));
+        return $this->render('index.html.twig');
     }
 }
 
