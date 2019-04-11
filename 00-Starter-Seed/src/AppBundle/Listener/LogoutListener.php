4ac9951fb94650f607b408756e67d2811757871b
Alexis Luque <aluque@indexlabs.com.uy>
2019-04-11 15:14:24 -0300

    Logout from Auth0 authorization server

---
+++ 00-Starter-Seed/src/AppBundle/Listener/LogoutListener.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace AppBundle\Listener;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Http\Logout\LogoutSuccessHandlerInterface;
+
+class LogoutListener implements LogoutSuccessHandlerInterface
+{
+
+    /**
+     * Creates a Response object to send upon a successful logout.
+     *
+     * @return Response never null
+     */
+    public function onLogoutSuccess(Request $request)
+    {
+        $returnTo = $request->getSchemeAndHttpHost();
+        $logoutUrl = sprintf(
+            'https://%s/v2/logout?client_id=%s&returnTo=%s',
+            getenv('AUTH0_DOMAIN'),
+            getenv('AUTH0_CLIENT_ID'),
+            $returnTo);
+        return new RedirectResponse($logoutUrl);
+    }
+}
